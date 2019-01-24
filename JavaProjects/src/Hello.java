9a987979a69bb1c5d55e32a179dcc7f78e00c476
Shraddha-Jagdale <46367206+Shraddha-Jagdale@users.noreply.github.com>
2019-01-24 12:36:10 +0530

    Revert "change occured"
    This reverts commit ffe90702c435099bbcdf538443f0c42e7981467e.

--- JavaProjects/src/Hello.java
+++ JavaProjects/src/Hello.java
@@ -7,11 +7,8 @@ public class Hello {
             System.out.println(i);
             System.out.println("Hellow ");
 System.out.println("Hellow12 ");
-<<<<<<< HEAD
 System.out.println("Hello Shraddha ");
 System.out.println("Hello Shraddha you are eTING MORE JUNK FOOD...PIZZA ");
-=======
->>>>>>> parent of adf5b55... Changed to Shraddha
         }
     }
 }
